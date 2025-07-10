7556d24c0ecdaacb74a613273bd939ad54c9b5e5
MSDNicrosoft <i@msdnicrosoft.work>
2025-07-10 13:35:44 +0800

    Fix colored message on Velocity
    Signed-off-by: MSDNicrosoft <i@msdnicrosoft.work>

--- autoworldmap-velocity/src/main/java/top/catowncraft/autoworldmap/velocity/command/CommandSenderImpl.java
+++ autoworldmap-velocity/src/main/java/top/catowncraft/autoworldmap/velocity/command/CommandSenderImpl.java
@@ -1,10 +1,12 @@
 package top.catowncraft.autoworldmap.velocity.command;
 
 import com.velocitypowered.api.command.CommandSource;
-import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import top.catowncraft.autoworldmap.common.command.ICommandSender;
 
 public class CommandSenderImpl implements ICommandSender {
+    private final static LegacyComponentSerializer LEGACY_SERIALIZER = LegacyComponentSerializer.legacySection();
+
     private final CommandSource source;
 
     public CommandSenderImpl(CommandSource source) {
@@ -13,12 +15,12 @@ public class CommandSenderImpl implements ICommandSender {
 
     @Override
     public void sendFeedback(String msg) {
-        this.source.sendMessage(Component.text(msg));
+        this.source.sendMessage(LEGACY_SERIALIZER.deserialize(msg));
     }
 
     @Override
     public void sendFeedback(String msg, Object... objects) {
-        this.source.sendMessage(Component.text(String.format(msg, objects)));
+        this.source.sendMessage(LEGACY_SERIALIZER.deserialize(String.format(msg, objects)));
     }
 
     @Override
